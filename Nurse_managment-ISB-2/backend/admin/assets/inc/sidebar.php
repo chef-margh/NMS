9c643f0f69e3d9a3c9b5312771128535803ae3af
AlarsTuring <joeashthomas@gmail.com>
2024-01-25 01:02:33 +0400

    added all folders

---
+++ Nurse_managment-ISB-2/backend/admin/assets/inc/sidebar.php
@@ -0,0 +1,179 @@
+<div class="left-side-menu">
+
+                <div class="slimscroll-menu">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+
+                        <ul class="metismenu" id="side-menu">
+
+                            <li class="menu-title">Navigation</li>
+
+                            <li>
+                                <a href="his_admin_dashboard.php">
+                                    <i class="fe-airplay"></i>
+                                    <span> Dashboard </span>
+                                </a>
+                                
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fab fa-accessible-icon "></i>
+                                    <span> Students </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_register_patient.php">Register Student</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_patients.php">View Students</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_patient.php">Manage Students</a>
+                                    </li>
+                                    <hr>
+                                  
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-doctor"></i>
+                                    <span> Nurse's </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_employee.php">Add Nurse</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_employee.php">View Nurse's</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_employee.php">Manage Nurse's</a>
+                                    </li>
+                                    <hr>
+                                    
+                                </ul>
+                            </li>
+
+                          
+
+                      
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class=" fas fa-funnel-dollar "></i>
+                                    <span> Inventory </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                   
+                                    <li>
+                                        <a href="his_admin_pharm_inventory.php">Pharmaceuticals</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="his_admin_equipments_inventory.php">Assets</a>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+                
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fe-share"></i>
+                                    <span> Reporting </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_inpatient_records.php">InPatient Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_outpatient_records.php">OutPatient Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_employee_records.php">Employee Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_pharmaceutical_records.php">Pharmaceutical Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_accounting_records.php">Accounting Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_medical_records.php">Medical Records</a>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fe-file-text"></i>
+                                    <span> Medical Records </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_medical_record.php">Add Medical Record</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_medical_record.php">Manage Medical Records</a>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-flask"></i>
+                                    <span> Laboratory </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_patient_lab_test.php">Student Ailment</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_patient_lab_result.php">Medicine Given</a>
+                                    </li>
+
+                                    
+                                    <li>
+                                        <a href="his_admin_lab_report.php">Incident Reports</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                    
+                                </ul>
+                            </li>
+
+                          
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fas fa-lock"></i>
+                                    <span> Password Resets </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_manage_password_resets.php">Manage</a>
+                                    </li>
+                                                                        
+                                </ul>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                    <!-- End Sidebar -->
+
+                    <div class="clearfix"></div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
