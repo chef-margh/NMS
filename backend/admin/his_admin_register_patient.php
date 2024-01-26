c976bf44a3cd973e43294634e3ba34e7d98efeb3
chef-margh <vinilsanjeth148@gmail.com>
2024-01-26 14:10:29 +0400

    dsfaewgrvesdfdas

--- backend/admin/his_admin_register_patient.php
+++ backend/admin/his_admin_register_patient.php
@@ -1,175 +1,191 @@
-<!--Server side code to handle  Patient Registration-->
+<!--Server side code to handle Patient Registration-->
 <?php
-	session_start();
-	include('assets/inc/config.php');
-		if(isset($_POST['add_patient']))
-		{
-			$pat_fname=$_POST['pat_fname'];
-			$pat_lname=$_POST['pat_lname'];
-			$pat_number=$_POST['pat_number'];
-            $pat_phone=$_POST['pat_phone'];
-           
-            $pat_age = $_POST['pat_age'];
-            $pat_dob = $_POST['pat_dob'];
-            
-            //sql to insert captured values
-			$query = "INSERT INTO his_patients (pat_fname, pat_lname, pat_age, pat_dob, pat_number, pat_phone, pat_type, pat_addr) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-            $stmt = $mysqli->prepare($query);
-            $rc = $stmt->bind_param('ssssssss', $pat_fname, $pat_lname, $pat_age, $pat_dob, $pat_number, $pat_phone, $pat_type, $pat_addr);
+session_start();
+include('assets/inc/config.php');
 
-			$stmt->execute();
-			/*
-			
-			*echo"<script>alert('Successfully Created Account Proceed To Log In ');</script>";
-			*/ 
-			//declare a varible which will be passed to alert function
-			if($stmt)
-			{
-				$success = "Student Details Added";
-			}
-			else {
-				$err = "Please Try Again Or Try Later";
-			}
-			
-			
-		}
+$success = $err = '';
+
+if (isset($_POST['add_patient'])) {
+    // Validate form data
+    if (
+        isset($_POST['pat_fname'], $_POST['pat_lname'], $_POST['pat_dob'], $_POST['pat_age'], $_POST['pat_number'], $_POST['pat_phone'])
+        && !empty($_POST['pat_fname'])
+        && !empty($_POST['pat_lname'])
+        && !empty($_POST['pat_dob'])
+        && !empty($_POST['pat_age'])
+        && !empty($_POST['pat_number'])
+        && !empty($_POST['pat_phone'])
+    ) {
+        // Sanitize input data
+        $pat_fname = $_POST['pat_fname'];
+        $pat_lname = $_POST['pat_lname'];
+        $pat_dob = $_POST['pat_dob'];
+        $pat_age = $_POST['pat_age'];
+        $pat_number = $_POST['pat_number'];
+        $pat_phone = $_POST['pat_phone'];
+
+        // Additional fields
+        $pat_type = ''; // Add appropriate value
+        $pat_addr = ''; // Add appropriate value
+
+        // SQL to insert captured values
+        $query = "INSERT INTO his_patients (pat_fname, pat_lname, pat_age, pat_dob, pat_number, pat_phone, pat_type, pat_addr) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $mysqli->prepare($query);
+        $rc = $stmt->bind_param('ssssssss', $pat_fname, $pat_lname, $pat_age, $pat_dob, $pat_number, $pat_phone, $pat_type, $pat_addr);
+
+        if ($stmt->execute()) {
+            $success = "Student Details Added";
+        } else {
+            $err = "Error: " . $stmt->error;
+        }
+    } else {
+        $err = "Please fill in all required fields.";
+    }
+}
 ?>
+
 <!--End Server Side-->
 <!--End Patient Registration-->
 <!DOCTYPE html>
 <html lang="en">
-    
-    <!--Head-->
-    <?php include('assets/inc/head.php');?>
-    <body>
 
-        <!-- Begin page -->
-        <div id="wrapper">
+<!--Head-->
+<?php include('assets/inc/head.php');?>
 
-            <!-- Topbar Start -->
-            <?php include("assets/inc/nav.php");?>
-            <!-- end Topbar -->
+<body>
 
-            <!-- ========== Left Sidebar Start ========== -->
-            <?php include("assets/inc/sidebar.php");?>
-            <!-- Left Sidebar End -->
+    <!-- Begin page -->
+    <div id="wrapper">
 
-            <!-- ============================================================== -->
-            <!-- Start Page Content here -->
-            <!-- ============================================================== -->
+        <!-- Topbar Start -->
+        <?php include("assets/inc/nav.php");?>
+        <!-- end Topbar -->
 
-            <div class="content-page">
-                <div class="content">
+        <!-- ========== Left Sidebar Start ========== -->
+        <?php include("assets/inc/sidebar.php");?>
+        <!-- Left Sidebar End -->
 
-                    <!-- Start Content-->
-                    <div class="container-fluid">
-                        
-                        <!-- start page title -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box">
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="his_admin_dashboard.php">Dashboard</a></li>
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Students</a></li>
-                                            <li class="breadcrumb-item active">Add Student</li>
-                                        </ol>
-                                    </div>
-                                    <h4 class="page-title">Add Student Details</h4>
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+
+        <div class="content-page">
+            <div class="content">
+
+                <!-- Start Content-->
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box">
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="his_admin_dashboard.php">Dashboard</a></li>
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Students</a></li>
+                                        <li class="breadcrumb-item active">Add Student</li>
+                                    </ol>
                                 </div>
+                                <h4 class="page-title">Add Student Details</h4>
                             </div>
-                        </div>     
-                        <!-- end page title --> 
-                        <!-- Form row -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h4 class="header-title">Fill all fields</h4>
-                                        <!--Add Patient Form-->
-                                        <form method="post">
-                                            <div class="form-row">
-                                                <div class="form-group col-md-6">
-                                                    <label for="inputEmail4" class="col-form-label">First Name</label>
-                                                    <input type="text" required="required" name="pat_fname" class="form-control" id="inputEmail4" placeholder="Student's First Name">
-                                                </div>
-                                                <div class="form-group col-md-6">
-                                                    <label for="inputPassword4" class="col-form-label">Last Name</label>
-                                                    <input required="required" type="text" name="pat_lname" class="form-control"  id="inputPassword4" placeholder="Student`s Last Name">
-                                                </div>
-                                            </div>
-
-                                            <div class="form-row">
-                                                <div class="form-group col-md-6">
-                                                    <label for="inputEmail4" class="col-form-label">Date Of Birth</label>
-                                                    <input type="text" required="required" name="pat_dob" class="form-control" id="inputEmail4" placeholder="DD/MM/YYYY">
-                                                </div>
-                                                <div class="form-group col-md-6">
-                                                    <label for="inputPassword4" class="col-form-label">Age</label>
-                                                    <input required="required" type="text" name="pat_age" class="form-control"  id="inputPassword4" placeholder="Student`s Age">
-                                                </div>
-                                            </div>
-
-                                            
-
-                                        
-                                                
-                                               
-                                                <div class="form-group col-md-2" style="display:none">
-                                                    <?php 
-                                                        $length = 5;    
-                                                        $patient_number =  substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'),1,$length);
-                                                    ?>
-                                                    <label for="inputZip" class="col-form-label">Student Number</label>
-                                                    <input type="text" name="pat_number" value="<?php echo $patient_number;?>" class="form-control" id="inputZip">
-                                                </div>
-                                            </div>
-
-                                            <button type="submit" name="add_patient" class="ladda-button btn btn-primary" data-style="expand-right">Add Student</button>
-
-                                        </form>
-                                        <!--End Patient Form-->
-                                    </div> <!-- end card-body -->
-                                </div> <!-- end card-->
-                            </div> <!-- end col -->
                         </div>
-                        <!-- end row -->
+                    </div>
+                    <!-- end page title -->
+                    <!-- Form row -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="header-title">Fill all fields</h4>
+                                    <!--Add Patient Form-->
+                                    <form method="post">
+                                        <div class="form-row">
+                                            <div class="form-group col-md-6">
+                                                <label for="inputEmail4" class="col-form-label">First Name</label>
+                                                <input type="text" required="required" name="pat_fname" class="form-control" id="inputEmail4"
+                                                    placeholder="Student's First Name">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="inputPassword4" class="col-form-label">Last Name</label>
+                                                <input required="required" type="text" name="pat_lname" class="form-control" id="inputPassword4"
+                                                    placeholder="Student`s Last Name">
+                                            </div>
+                                        </div>
 
-                    </div> <!-- container -->
+                                        <div class="form-row">
+                                            <div class="form-group col-md-6">
+                                                <label for="inputEmail4" class="col-form-label">Date Of Birth</label>
+                                                <input type="text" required="required" name="pat_dob" class="form-control" id="inputEmail4"
+                                                    placeholder="DD/MM/YYYY">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="inputPassword4" class="col-form-label">Age</label>
+                                                <input required="required" type="text" name="pat_age" class="form-control" id="inputPassword4"
+                                                    placeholder="Student`s Age">
+                                            </div>
+                                        </div>
 
-                </div> <!-- content -->
+                                        <div class="form-group col-md-2" style="display:none">
+                                            <?php
+                                            $length = 5;
+                                            $patient_number = substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 1, $length);
+                                            ?>
+                                            <label for="inputZip" class="col-form-label">Student Number</label>
+                                            <input type="text" name="pat_number" value="<?php echo $patient_number; ?>" class="form-control" id="inputZip">
+                                        </div>
+                                        </div>
 
-                <!-- Footer Start -->
-                <?php include('assets/inc/footer.php');?>
-                <!-- end Footer -->
+                                        <button type="submit" name="add_patient" class="ladda-button btn btn-primary" data-style="expand-right">Add Student</button>
 
-            </div>
+                                    </form>
+                                    <!--End Patient Form-->
+                                    <?php
+                                    if ($success) {
+                                        echo '<div class="alert alert-success">' . $success . '</div>';
+                                    }
+                                    if ($err) {
+                                        echo '<div class="alert alert-danger">' . $err . '</div>';
+                                    }
+                                    ?>
+                                </div> <!-- end card-body -->
+                            </div> <!-- end card-->
+                        </div> <!-- end col -->
+                    </div>
+                    <!-- end row -->
 
-            <!-- ============================================================== -->
-            <!-- End Page content -->
-            <!-- ============================================================== -->
+                </div> <!-- container -->
 
+            </div> <!-- content -->
+
+            <!-- Footer Start -->
+            <?php include('assets/inc/footer.php');?>
+            <!-- end Footer -->
 
         </div>
-        <!-- END wrapper -->
 
-       
-        <!-- Right bar overlay-->
-        <div class="rightbar-overlay"></div>
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
 
-        <!-- Vendor js -->
-        <script src="assets/js/vendor.min.js"></script>
+    </div>
+    <!-- END wrapper -->
 
-        <!-- App js-->
-        <script src="assets/js/app.min.js"></script>
+    <!-- Right bar overlay-->
+    <div class="rightbar-overlay"></div>
 
-        <!-- Loading buttons js -->
-        <script src="assets/libs/ladda/spin.js"></script>
-        <script src="assets/libs/ladda/ladda.js"></script>
+    <!-- Vendor js -->
+    <script src="assets/js/vendor.min.js"></script>
 
-        <!-- Buttons init js-->
-        <script src="assets/js/pages/loading-btn.init.js"></script>
-        
-    </body>
+    <!-- App js-->
+    <script src="assets/js/app.min.js"></script>
+
+    <!-- Loading buttons js -->
+    <script src="assets/libs/ladda/spin.js"></script>
+    <script src="assets/libs/ladda/ladda.js"></script>
+
+    <!-- Buttons init js-->
+    <script src="assets/js/pages/loading-btn.init.js"></script>
+
+</body>
 
 </html>
