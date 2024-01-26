2b43b9b1a9264dc5c07d93b9cc3acb76e23ec13c
chef-margh <vinilsanjeth148@gmail.com>
2024-01-26 11:37:32 +0400

    effewrwerew

--- backend/doc/assets/inc/sidebar.php
+++ backend/doc/assets/inc/sidebar.php
@@ -1,78 +1,140 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Page Title</title>
-    
-    <!-- Include required CSS styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/metisMenu/3.0.6/metisMenu.min.css">
+<div class="left-side-menu">
 
-    <!-- Include required JavaScript libraries -->
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/metisMenu/3.0.6/metisMenu.min.js"></script>
+                <div class="slimscroll-menu">
 
-    <!-- Initialize MetisMenu -->
-    <script>
-        $(document).ready(function () {
-            $('#side-menu').metisMenu();
-        });
-    </script>
-</head>
-<body>
-    <div class="left-side-menu">
-        <div class="slimscroll-menu">
-            <!--- Sidemenu -->
-            <div id="sidebar-menu">
-                <ul class="metismenu" id="side-menu">
-                    <li class="menu-title">Navigation</li>
-                    
-                    <li>
-                        <a href="his_admin_dashboard.php">
-                            <i class="fe-airplay"></i>
-                            <span> Home </span>
-                        </a>
-                    </li>
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
 
-                    <li>
-                        <a href="javascript: void(0);">
-                            <i class="fab fa-accessible-icon "></i>
-                            <span> Students </span>
-                            <span class="menu-arrow"></span>
-                        </a>
-                        <ul class="nav-second-level" aria-expanded="false">
+                        <ul class="metismenu" id="side-menu">
+
+                            <li class="menu-title">Navigation</li>
+
                             <li>
-                                <a href="his_admin_register_patient.php">Register Student</a>
+                                <a href="his_admin_dashboard.php">
+                                    <i class="fe-airplay"></i>
+                                    <span> Home </span>
+                                </a>
+                                
                             </li>
+
                             <li>
-                                <a href="his_admin_view_patients.php">View Students</a>
+                                <a href="javascript: void(0);">
+                                    <i class="fab fa-accessible-icon "></i>
+                                    <span> Patients </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_register_patient.php">Register Student</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_patients.php">View Students</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_patient.php">Manage Students</a>
+                                    </li>
+                                    <hr>
+                                </ul>
                             </li>
+
+                          
+
                             <li>
-                                <a href="his_admin_manage_patient.php">Manage Students</a>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-pill"></i>
+                                    <span> Pharmacy </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_pharm_cat.php">Add Pharm Category</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_pharm_cat.php">View Pharm Category</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_pharm_cat.php">Manage Pharm Category</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_add_pharmaceuticals.php">Add Pharmaceuticals</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_pharmaceuticals.php">View Pharmaceuticals</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_pharmaceuticals.php">Manage Pharmaceuticals</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_add_presc.php">Add Prescriptions</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_presc.php">View Prescriptions</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_presc.php">Manage Prescriptions</a>
+                                    </li>
+                                </ul>
                             </li>
+
+                            
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class=" fas fa-funnel-dollar "></i>
+                                    <span> Inventory </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                   
+                                    <li>
+                                        <a href="his_admin_pharm_inventory.php">Pharmaceuticals</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="his_admin_equipments_inventory.php">Assets</a>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+                
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-flask"></i>
+                                    <span> Report </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_patient_lab_test.php">Student Ailment</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_patient_lab_result.php">Treatment Given</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_patient_lab_vitals.php">Incident Report</a>
+                                    </li>
+                                    
+                                    <li>
+                                        <a href="his_admin_lab_report.php">Lab Reports</a>
+                                    </li>
+                                    <hr>
+                                    
+                                </ul>
+                            </li>
+
+                           
+
+                            
+
                         </ul>
-                    </li>
 
-                    <!-- ... other menu items ... -->
+                    </div>
+                    <!-- End Sidebar -->
 
-                    <li>
-                        <a href="javascript: void(0);">
-                            <i class="fas fa-lock"></i>
-                            <span> Password Resets </span>
-                            <span class="menu-arrow"></span>
-                        </a>
-                        <ul class="nav-second-level" aria-expanded="false">
-                            <li>
-                                <a href="his_admin_manage_password_resets.php">Manage</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+                    <div class="clearfix"></div>
+
+                </div>
+                <!-- Sidebar -left -->
+
             </div>
-            <!-- End Sidebar -->
-            <div class="clearfix"></div>
-        </div>
-        <!-- Sidebar -left -->
-    </div>
-</body>
-</html>
