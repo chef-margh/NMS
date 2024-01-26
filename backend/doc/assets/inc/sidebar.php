c5535c895e1330f8fdf1d744594a9201e307c2fe
chef-margh <vinilsanjeth148@gmail.com>
2024-01-26 11:30:03 +0400

    dcsqcsqcdacqwc

--- backend/doc/assets/inc/sidebar.php
+++ backend/doc/assets/inc/sidebar.php
@@ -12,7 +12,7 @@
                             <li>
                                 <a href="his_doc_dashboard.php">
                                     <i class="fe-airplay"></i>
-                                    <span> Dashboard </span>
+                                    <span> Home </span>
                                 </a>
                                 
                             </li>
@@ -25,21 +25,15 @@
                                 </a>
                                 <ul class="nav-second-level" aria-expanded="false">
                                     <li>
-                                        <a href="his_doc_register_patient.php">Register Patient</a>
+                                        <a href="his_doc_register_patient.php">Register Student</a>
                                     </li>
                                     <li>
-                                        <a href="his_doc_view_patients.php">View Patients</a>
+                                        <a href="his_doc_view_patients.php">View Students</a>
                                     </li>
                                     <li>
-                                        <a href="his_doc_manage_patient.php">Manage Patients</a>
+                                        <a href="his_doc_manage_patient.php">Manage Students</a>
                                     </li>
                                     <hr>
-                                    <li>
-                                        <a href="his_doc_discharge_patient.php">Discharge Patients</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_doc_patient_transfer.php">Patient Transfers</a>
-                                    </li>
                                 </ul>
                             </li>
 
@@ -107,18 +101,18 @@
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class="mdi mdi-flask"></i>
-                                    <span> Laboratory </span>
+                                    <span> Report </span>
                                     <span class="menu-arrow"></span>
                                 </a>
                                 <ul class="nav-second-level" aria-expanded="false">
                                     <li>
-                                        <a href="his_doc_patient_lab_test.php">Patient Lab Tests</a>
+                                        <a href="his_doc_patient_lab_test.php">Student Ailment</a>
                                     </li>
                                     <li>
-                                        <a href="his_doc_patient_lab_result.php">Patient Lab Results</a>
+                                        <a href="his_doc_patient_lab_result.php">Treatment Given</a>
                                     </li>
                                     <li>
-                                        <a href="his_doc_patient_lab_vitals.php">Patient Vitals</a>
+                                        <a href="his_doc_patient_lab_vitals.php">Incident Report</a>
                                     </li>
                                     
                                     <li>
@@ -129,19 +123,7 @@
                                 </ul>
                             </li>
 
-                            <li>
-                                <a href="javascript: void(0);">
-                                    <i class="mdi mdi-cash-refund "></i>
-                                    <span> Payrolls </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul class="nav-second-level" aria-expanded="false">
-                                    
-                                    <li>
-                                        <a href="his_doc_view_payrolls.php">My Payrolls</a>
-                                    </li>
-                                </ul>
-                            </li>
+                           
 
                             
 
