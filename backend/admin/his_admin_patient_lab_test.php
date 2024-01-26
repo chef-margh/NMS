f2f0ab5815eec6ea31658d72d40ef425f4dcc8a6
chef-margh <vinilsanjeth148@gmail.com>
2024-01-26 15:48:56 +0400

    tgrefgs

--- backend/admin/his_admin_patient_lab_test.php
+++ backend/admin/his_admin_patient_lab_test.php
@@ -41,8 +41,8 @@
                                     <div class="page-title-right">
                                         <ol class="breadcrumb m-0">
                                             <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Laboratory</a></li>
-                                            <li class="breadcrumb-item active">Add Laboratory Test</li>
+                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Report</a></li>
+                                            <li class="breadcrumb-item active">Student Details  </li>
                                         </ol>
                                     </div>
                                     <h4 class="page-title">Student Ailment</h4>
