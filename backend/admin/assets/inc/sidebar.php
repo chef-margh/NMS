6aee2fc8ed307274104e9bc732ff6590f8253477
chef-margh <vinilsanjeth148@gmail.com>
2024-01-26 10:29:22 +0400

    barchange

--- backend/admin/assets/inc/sidebar.php
+++ backend/admin/assets/inc/sidebar.php
@@ -12,7 +12,7 @@
                             <li>
                                 <a href="his_admin_dashboard.php">
                                     <i class="fe-airplay"></i>
-                                    <span> Dashboard </span>
+                                    <span> Home </span>
                                 </a>
                                 
                             </li>
@@ -20,53 +20,41 @@
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class="fab fa-accessible-icon "></i>
-                                    <span> Patients </span>
+                                    <span> Students </span>
                                     <span class="menu-arrow"></span>
                                 </a>
                                 <ul class="nav-second-level" aria-expanded="false">
                                     <li>
-                                        <a href="his_admin_register_patient.php">Register Patient</a>
+                                        <a href="his_admin_register_patient.php">Register Student</a>
                                     </li>
                                     <li>
-                                        <a href="his_admin_view_patients.php">View Patients</a>
+                                        <a href="his_admin_view_patients.php">View Students</a>
                                     </li>
                                     <li>
-                                        <a href="his_admin_manage_patient.php">Manage Patients</a>
+                                        <a href="his_admin_manage_patient.php">ManageStudents</a>
                                     </li>
                                     <hr>
-                                    <li>
-                                        <a href="his_admin_discharge_patient.php">Discharge Patients</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_patient_transfer.php">Patient Transfers</a>
-                                    </li>
+                                   
                                 </ul>
                             </li>
 
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class="mdi mdi-doctor"></i>
-                                    <span> Employees </span>
+                                    <span> Nurses </span>
                                     <span class="menu-arrow"></span>
                                 </a>
                                 <ul class="nav-second-level" aria-expanded="false">
                                     <li>
-                                        <a href="his_admin_add_employee.php">Add Employee</a>
+                                        <a href="his_admin_add_employee.php">Add Nurse</a>
                                     </li>
                                     <li>
-                                        <a href="his_admin_view_employee.php">View Employees</a>
+                                        <a href="his_admin_view_employee.php">View Nurses</a>
                                     </li>
                                     <li>
-                                        <a href="his_admin_manage_employee.php">Manage Employees</a>
+                                        <a href="his_admin_manage_employee.php">Manage Nurses</a>
                                     </li>
-                                    <hr>
-                                    <li>
-                                        <a href="his_admin_assaign_dept.php">Assign Department</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_transfer_employee.php">Transfer Employee</a>
-                                    </li>
-                                </ul>
+                                  
                             </li>
 
                             <li>
@@ -96,38 +84,9 @@
                                         <a href="his_admin_manage_pharmaceuticals.php">Manage Pharmaceuticals</a>
                                     </li>
                                     <hr>
-                                    <li>
-                                        <a href="his_admin_add_presc.php">Add Prescriptions</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_view_presc.php">View Prescriptions</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_manage_presc.php">Manage Prescriptions</a>
-                                    </li>
-                                </ul>
-                            </li>
+                                 
 
-                            <li>
-                                <a href="javascript: void(0);">
-                                    <i class="mdi mdi-cash-multiple "></i>
-                                    <span> Accounting </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_admin_add_acc.payable.php">Add Acc. Payable</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_manage_acc_payable.php">Manage Acc. Payable</a>
-                                    </li>
-                                    <hr>
-                                    <li>
-                                        <a href="his_admin_add_acc_receivable.php">Add Acc. Receivable</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_manage_acc_receivable.php">Manage Acc. Receivable</a>
-                                    </li>
+                          
                                     <hr>
                                     
                                     
@@ -152,34 +111,6 @@
                                 </ul>
                             </li>
                 
-                            <li>
-                                <a href="javascript: void(0);">
-                                    <i class="fe-share"></i>
-                                    <span> Reporting </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_admin_inpatient_records.php">InPatient Records</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_outpatient_records.php">OutPatient Records</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_employee_records.php">Employee Records</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_pharmaceutical_records.php">Pharmaceutical Records</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_accounting_records.php">Accounting Records</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_medical_records.php">Medical Records</a>
-                                    </li>
-                                    
-                                </ul>
-                            </li>
 
                             <li>
                                 <a href="javascript: void(0);">
@@ -201,22 +132,20 @@
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class="mdi mdi-flask"></i>
-                                    <span> Laboratory </span>
+                                    <span> Report </span>
                                     <span class="menu-arrow"></span>
                                 </a>
                                 <ul class="nav-second-level" aria-expanded="false">
                                     <li>
-                                        <a href="his_admin_patient_lab_test.php">Patient Lab Tests</a>
+                                        <a href="his_admin_patient_lab_test.php">Student Ailment</a>
                                     </li>
                                     <li>
-                                        <a href="his_admin_patient_lab_result.php">Patient Lab Results</a>
+                                        <a href="his_admin_patient_lab_result.php">Treatment Given</a>
                                     </li>
                                     <li>
-                                        <a href="his_admin_patient_lab_vitals.php">Patient Vitals</a>
+                                        <a href="his_admin_patient_lab_vitals.php">Incident Report</a>
                                     </li>
-                                    <li>
-                                        <a href="his_admin_employee_lab_vitals.php">Employee Vitals</a>
-                                    </li>
+                                    
                                     <li>
                                         <a href="his_admin_lab_report.php">Lab Reports</a>
                                     </li>
@@ -230,64 +159,7 @@
                                 </ul>
                             </li>
 
-                            <li>
-                                <a href="javascript: void(0);">
-                                    <i class="mdi mdi-scissors-cutting "></i>
-                                    <span> Surgical / Theatre </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_admin_add_equipment.php">Add Equipment</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_manage_equipment.php">Manage Equipments</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_add_theatre_patient.php">Add Patient</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_manage_theatre_patient.php">Manage Patients</a>
-                                    </li>
-
-                                    <li>
-                                        <a href="his_admin_surgery_records.php">Surgery Records</a>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <a href="javascript: void(0);">
-                                    <i class="mdi mdi-cash-refund "></i>
-                                    <span> Payrolls </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_admin_add_payroll.php">Add Payroll</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_manage_payrolls.php">Manage Payrolls</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_generate_payrolls.php">Generate Payrolls</a>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <a href="javascript: void(0);">
-                                    <i class="fas fa-user-tag"></i>
-                                    <span> Vendors </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_admin_add_vendor.php">Add Vendor</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_manage_vendor.php">Manage Vendors</a>
-                                    </li>
+              
                                     
                                 </ul>
                             </li>
@@ -315,4 +187,5 @@
                 </div>
                 <!-- Sidebar -left -->
 
+            </div>
             </div>
